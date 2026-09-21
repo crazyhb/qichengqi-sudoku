9de31f7999806935c0dfc5cdd9767362e12a5372
crazyhb <50038534+crazyhb@users.noreply.github.com>
2026-09-21 19:59:56 +0800

    Publish 7x7 Sudoku static site

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'qichengqi-sudoku-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(['./', './index.html', './favicon.svg', './manifest.webmanifest'])),
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))),
+    ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetched = fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => cached || caches.match('./index.html'));
+      return cached || fetched;
+    }),
+  );
+});
